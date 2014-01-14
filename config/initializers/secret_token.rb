ddb79867ef9093d148c784e7902c0716830f7448
Mark Hurwitz <mhurwitzus@gmail.com>
2014-01-14 16:55:57 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmailAdventure::Application.config.secret_key_base = 'b67c62f17dbb4bf2537ee2bec93cc360a9fb085a7f3a8135c53d53fb4c75dbab513bf79bf6900051e79482f8a6e987c1d5acb76103ce49b7e1fc3d2edd749c56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmailAdventure::Application.config.session_store :cookie_store, key: '_email_adventure_session'
