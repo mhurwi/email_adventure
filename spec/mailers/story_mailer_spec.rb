53cfb2db8616872f2e20ba1d9bb839e54625d1a4
Mark Hurwitz <mhurwitzus@gmail.com>
2014-02-15 18:43:14 -0600

    building StoryMailer#confirm_start

--- spec/mailers/story_mailer_spec.rb
+++ spec/mailers/story_mailer_spec.rb
@@ -3,26 +3,42 @@ require "spec_helper"
 describe StoryMailer do
 	let(:user) { FactoryGirl.create(:user)}
 	let(:story) { FactoryGirl.create(:story) }
-	let(:player_account) { OpenStruct.new(
-		email: "player@example.com"
+	let(:player_account) { PlayerAccount.new(
+		email: "player@example.com",
+		confirmation_token: 12345
 		)} 	
  	let(:confirm_start_email) { StoryMailer.confirm_start(
  				story, player_account
  				)}
+ 	let(:sent_email) { ActionMailer::Base.deliveries.first }
 
  	before :each do 
- 		email_account
- 		choice
- 		choice2
+ 		ActionMailer::Base.deliveries = []
+ 		player_account
+ 		story
  		confirm_start_email.deliver
  	end
 
 	context "confirm_start" do 
-		it "emails player the confirm_start email"
+		it "emails player the confirm_start email" do 
+			expect(ActionMailer::Base.deliveries.count).to eql 1
+		end
 		it "uses the app deafault SMPT settings"
-		it "includes link to confirm_start"
-		it "includes confirmation_token as param in link"
-		it "includes player_account_id in link"
-		it "includes story_id in link"
+		it "includes link to confirm_start" do 
+			expect(sent_email.body).to match confirm_start_api_v1_stories_path
+		end
+		it "includes confirmation_token" do 
+			expect(sent_email.body).to match "12345"
+		end
+		it "includes player_account_id" do 
+			expect(sent_email.body).to match player_account.id.to_s
+		end
+		it "includes story_id" do 
+			expect(sent_email.body).to match story.id.to_s
+		end
+		context "SMTP settings" do 
+			it "uses correct default SMTP settings"
+			it "uses the correct from addess"
+		end
  	end
 end
